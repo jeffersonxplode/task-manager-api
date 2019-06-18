939bbd78e1324e5ae74a12be8005ca8499a2a543
xplode <jeffersonxplode@gmail.com>
2019-06-18 20:39:25 -0300

    Adicionando os teste iniciais.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,11 @@
 
-FactoryGirl.define do
+FactoryBot.define do
 
   factory :user do
 
+    email  {Faker::Internet.email}
+    password {"123456"}
+    password_confirmation {"123456"}
   end
 
 end
